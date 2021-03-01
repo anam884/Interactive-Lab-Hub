d4c47a350bf70af9c8ee3f4bd7e393aea47853c4
anam884 <53217164+anam884@users.noreply.github.com>
2021-02-28 22:16:04 -0500

    Update test.py

--- Lab 2/test.py
+++ Lab 2/test.py
@@ -130,7 +130,7 @@ while True:
 # #     disp.image(image,rotation)
              image2 = Image.open(f"{i}.png")
 #              image_scale = scale_image(image2)
-             disp.image(image2,rotation)
+             disp.image(image2)
 #              draw.text((x1,y1), time.strftime("%a %d" ), font=font1, fill="#FF0FF0")
 #              draw.text((x2,y2), time.strftime("%H:%M"), font=font2, fill="#FF0FF0")
 #              disp.image(image,rotation)
