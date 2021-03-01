97e17b57706efdc377dc34b85a981760f9c09c76
anam884 <53217164+anam884@users.noreply.github.com>
2021-02-28 20:58:12 -0500

    Update test.py

--- Lab 2/test.py
+++ Lab 2/test.py
@@ -131,7 +131,7 @@ while True:
              draw.text((x1,y1), time.strftime("%a %d" ), font=font1, fill="#FF0FF0")
              draw.text((x2,y2), time.strftime("%H:%M"), font=font2, fill="#FF0FF0")
              disp.image(image,rotation)
-             draw.rectangle((0, 0, width, height), outline=0, fill="#FFFFFF")
+             draw.rectangle((0, 0, width, height), outline=0, fill=None)
              draw.text((x1,y1), time.strftime("%a %d" ), font=font1, fill="#FF0FF0")
              draw.text((x2,y2), time.strftime("%H:%M"), font=font2, fill="#FF0FF0")
              disp.image(image,rotation)
